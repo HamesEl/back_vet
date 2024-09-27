bbb6bc094ce0a205b7015f792f91074ae6ef9df8
Harold Elías <129306594+HamesEl@users.noreply.github.com>
2024-09-26 23:46:23 -0500

    Update WebConfig.java

--- vet_back_con/src/main/java/com/vetconnect/vetconnect/config/WebConfig.java
+++ vet_back_con/src/main/java/com/vetconnect/vetconnect/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.vetconnect.vetconnect.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +12,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/META-INF/resources/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permite todas las rutas
+                .allowedOrigins("https://accurate-adventure-production.up.railway.app") // Reemplaza con tu dominio
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+                .allowedHeaders("*"); // Todos los headers permitidos
     }
 }
